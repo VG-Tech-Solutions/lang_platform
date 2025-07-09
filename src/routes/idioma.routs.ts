c1e070fd4c89b5d4670cd58b74e0556f824745c0
GustavPcosta <onepim@hotmail.com>
2025-07-08 22:48:03 -0300

    update_routs

--- src/routes/idioma.routs.ts
+++ src/routes/idioma.routs.ts
@@ -5,12 +5,7 @@ import { LangNativeController } from '../controllers/idiomaController';
 const router = Router();
 const langNativeController = new LangNativeController();
 
-/**
- * @route GET /api/lang-natives
- * @description Listar todos os idiomas nativos ordenados por native_order
- * @access Public
- */
-router.get('/', langNativeController.getAllLangNatives);
+router.get('/lang/native', langNativeController.getAllLangNatives);
 
 
 
